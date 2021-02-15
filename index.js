112a9cd40693d953d8c821de0a41af3d0553ae05
Michael Fuget <michaelfuget@gmail.com>
2021-02-15 00:43:42 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // Your code here
+class Cat {
+  //...
+  constructor(name, sex)
+  {
+    this.name = name;
+    this.sex = sex;
+  }
+  speak(){
+    return this.name + "says meow!".
+  }
+  
+}
+ 
+class Dog {
+  //...
+}
+ 
+class Bird {
+  //...
+}
